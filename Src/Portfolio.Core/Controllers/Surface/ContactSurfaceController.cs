b06bb7f6938d40496386aa8be0199bf1c4dfa6dc
Abe Kasper <abe.kasper@hva.nl>
2022-06-09 23:21:18 +0200

    Activity 7 Done

--- Src/Portfolio.Core/Controllers/Surface/ContactSurfaceController.cs
+++ Src/Portfolio.Core/Controllers/Surface/ContactSurfaceController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Portfolio.Core.Models.FormModels;
+using Portfolio.Core.Services;
 using Umbraco.Cms.Core.Cache;
 using Umbraco.Cms.Core.Logging;
 using Umbraco.Cms.Core.Routing;
@@ -12,6 +13,8 @@ namespace Portfolio.Core.Controllers.Surface
 {
     public class ContactSurfaceController: SurfaceController
     {
+        private readonly ITestService _testService;
+        
         [HttpPost]
         public IActionResult Submit([Bind(Prefix = "contactFormModel")] ContactFormModel formModel)
         {
@@ -19,11 +22,12 @@ namespace Portfolio.Core.Controllers.Surface
             {
                 return CurrentUmbracoPage();
             }
+            _testService.Add($"{formModel.Name} | {formModel.Email} |{formModel.Comment}");
             return RedirectToCurrentUmbracoPage();
         }
-        
-        public ContactSurfaceController(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory, ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider) : base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
+        public ContactSurfaceController(IUmbracoContextAccessor umbracoContextAccessor, ITestService testService, IUmbracoDatabaseFactory databaseFactory, ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider) : base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
         {
+            _testService = testService;
         }
     }
 }
---
+++ Src/Portfolio.Core/Services/TestService.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Portfolio.Core.Services
+{
+    public interface ITestService
+    {
+        public List<string> GetAll();
+        public void Add(string text);
+    }
+    public class TestService : ITestService
+    {
+        private readonly List<string> _texts = new List<string>();
+        public List<string> GetAll()
+        {
+            return _texts.ToList();
+        }
+        public void Add(string text)
+        {
+            _texts.Add(text);
+        }
+    }
+}
