54dccd530d96211f585725fe7717f589d995b8c6
Abe Kasper <abe.kasper@hva.nl>
2022-06-09 22:40:43 +0200

    activity 6 done

--- Src/Portfolio.Core/Controllers/Render/HomeController.cs
+++ Src/Portfolio.Core/Controllers/Render/HomeController.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ViewEngines;
 using Microsoft.Extensions.Logging;
+using Portfolio.Core.Extensions;
+using Portfolio.Core.Models.Umbraco;
+using Portfolio.Core.Models.ViewModels;
 using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.Web;
 using Umbraco.Cms.Web.Common.Controllers;
@@ -11,7 +15,12 @@ namespace Portfolio.Core.Controllers.Render
     {
         public IActionResult Home()
         {
-            return CurrentTemplate(new ContentModel(CurrentPage));
+            var home = (Home)CurrentPage;
+            var viewModel = new HomeViewModel();
+            viewModel.Build(CurrentPage);
+            viewModel.BodyText = new HtmlString(home.BodyText?.ToHtmlString() ??
+                                                string.Empty);
+            return CurrentTemplate(viewModel);
         }
         
         public HomeController(ILogger<RenderController> logger, ICompositeViewEngine compositeViewEngine, IUmbracoContextAccessor umbracoContextAccessor) : base(logger, compositeViewEngine, umbracoContextAccessor)
