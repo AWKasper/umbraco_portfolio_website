dac5e257a1e5575030b1882d59ba5fcbd3408f3f
Abe Kasper <abe.kasper@hva.nl>
2022-06-09 23:21:55 +0200

    Activity 7 Done

---
+++ Src/Portfolio.Core/Models/Umbraco/Components/StartupComposer.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Portfolio.Core.Services;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.DependencyInjection;
+
+namespace Portfolio.Core.Models.Umbraco.Components
+{
+    public class StartupComposer: IComposer
+    {
+        public void Compose(IUmbracoBuilder builder)
+        {
+            builder.Services.AddSingleton<ITestService, TestService>();
+            throw new System.NotImplementedException();
+        }
+    }
+}
