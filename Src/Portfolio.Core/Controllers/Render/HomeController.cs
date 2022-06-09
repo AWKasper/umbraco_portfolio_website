ce528e7d1bf3c784b6cea6bedfb6ba6364f29f02
Abe Kasper <abe.kasper@hva.nl>
2022-06-09 17:53:53 +0200

    halfway workbook 2

---
+++ Src/Portfolio.Core/Controllers/Render/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.Extensions.Logging;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Web.Common.Controllers;
+
+namespace Portfolio.Core.Controllers.Render
+{
+    public class HomeController: RenderController
+    {
+        public IActionResult Home()
+        {
+            return CurrentTemplate(new ContentModel(CurrentPage));
+        }
+        
+        public HomeController(ILogger<RenderController> logger, ICompositeViewEngine compositeViewEngine, IUmbracoContextAccessor umbracoContextAccessor) : base(logger, compositeViewEngine, umbracoContextAccessor)
+        {
+            
+        }
+    }
+}
