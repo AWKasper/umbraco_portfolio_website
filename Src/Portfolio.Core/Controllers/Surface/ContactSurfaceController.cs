f1fe08abbecd5c0b057b59665e069c20d05f5404
Abe Kasper <abe.kasper@hva.nl>
2022-06-09 23:52:32 +0200

    Activity 9 Done

--- Src/Portfolio.Core/Controllers/Surface/ContactSurfaceController.cs
+++ Src/Portfolio.Core/Controllers/Surface/ContactSurfaceController.cs
@@ -3,6 +3,8 @@ using Portfolio.Core.Models.FormModels;
 using Portfolio.Core.Services;
 using Umbraco.Cms.Core.Cache;
 using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Mail;
+using Umbraco.Cms.Core.Models.Email;
 using Umbraco.Cms.Core.Routing;
 using Umbraco.Cms.Core.Services;
 using Umbraco.Cms.Core.Web;
@@ -14,6 +16,7 @@ namespace Portfolio.Core.Controllers.Surface
     public class ContactSurfaceController: SurfaceController
     {
         private readonly ITestService _testService;
+        private readonly IEmailSender _emailSender;
         
         [HttpPost]
         public IActionResult Submit([Bind(Prefix = "contactFormModel")] ContactFormModel formModel)
@@ -23,11 +26,20 @@ namespace Portfolio.Core.Controllers.Surface
                 return CurrentUmbracoPage();
             }
             _testService.Add($"{formModel.Name} | {formModel.Email} |{formModel.Comment}");
+            var email = new EmailMessage(
+                formModel.Email,
+                "abe.kasper@hva.nl",
+                "Contact form",
+                $"{formModel.Name ?? "Unknown"}: {formModel.Comment}",
+                false
+            );
+            _emailSender.SendAsync(email, string.Empty);
             return RedirectToCurrentUmbracoPage();
         }
-        public ContactSurfaceController(IUmbracoContextAccessor umbracoContextAccessor, ITestService testService, IUmbracoDatabaseFactory databaseFactory, ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider) : base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
+        public ContactSurfaceController(IUmbracoContextAccessor umbracoContextAccessor, IEmailSender emailSender, ITestService testService, IUmbracoDatabaseFactory databaseFactory, ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider) : base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
         {
             _testService = testService;
+            _emailSender = emailSender;
         }
     }
 }
