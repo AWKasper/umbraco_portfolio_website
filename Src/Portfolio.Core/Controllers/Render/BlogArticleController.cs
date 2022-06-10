6c6956b44b1126b23432085e0aeff5a3b30afa2e
Abe Kasper <abe.kasper@hva.nl>
2022-06-10 10:21:51 +0200

    blogarticle viewmodel and controller

---
+++ Src/Portfolio.Core/Controllers/Render/BlogArticleController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using HeyRed.MarkdownSharp;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.Extensions.Logging;
+using Portfolio.Core.Extensions;
+using Portfolio.Core.Models.Umbraco;
+using Portfolio.Core.Models.ViewModels;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Web.Common.Controllers;
+using Umbraco.Extensions;
+
+namespace Portfolio.Core.Controllers.Render
+{
+    public class BlogArticleController: RenderController
+    {
+        public IActionResult BlogArticle()
+        {
+            var blogArticle = (BlogArticle)CurrentPage;
+            var viewModel = new BlogArticleViewModel();
+            viewModel.Build(CurrentPage);
+            viewModel.Title = new HtmlString(blogArticle.Title?.ToHtmlString() ??
+                                                string.Empty);
+            viewModel.Author = new HtmlString(blogArticle.Author?.ToHtmlString() ??
+                                                string.Empty);
+            viewModel.PublishDate = new HtmlString(blogArticle.PublishDate.ToIsoString()??
+                                                   string.Empty);
+            viewModel.AuthorPicture = new HtmlString(blogArticle.AuthorPicture?.GetCropUrl() ??
+                                                string.Empty);
+            viewModel.BodyText = new HtmlString(blogArticle.BodyText?.ToHtmlString() ??
+                                                string.Empty);
+            return CurrentTemplate(viewModel);
+        }
+        
+        public BlogArticleController(ILogger<RenderController> logger, ICompositeViewEngine compositeViewEngine, IUmbracoContextAccessor umbracoContextAccessor) : base(logger, compositeViewEngine, umbracoContextAccessor)
+        {
+        }
+    }
+}
---
+++ Src/Portfolio.Core/Models/ViewModels/BlogArticleViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Html;
+
+namespace Portfolio.Core.Models.ViewModels
+{
+    public class BlogArticleViewModel: MainViewModel
+    {
+        public HtmlString Title { get; set; }
+        public HtmlString Author { get; set; }
+        public HtmlString PublishDate { get; set; }
+        public HtmlString AuthorPicture { get; set; }
+        public HtmlString BodyText { get; set; }
+    }
+}
