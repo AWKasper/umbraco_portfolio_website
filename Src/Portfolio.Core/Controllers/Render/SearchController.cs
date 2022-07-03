de563255e1314a9274e46726f487a2a5a556671c
Abe Kasper <abe.kasper@hva.nl>
2022-07-03 16:15:55 +0200

    Making search page

---
+++ Src/Portfolio.Core/Controllers/Render/SearchController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.Extensions.Logging;
+using Portfolio.Core.Extensions;
+using Portfolio.Core.Models.Umbraco;
+using Portfolio.Core.Models.ViewModels;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Web.Common.Controllers;
+
+namespace Portfolio.Core.Controllers.Render
+{
+    public class SearchController : RenderController
+    {
+        
+        public IActionResult Search()
+        {
+            var search = (Search)CurrentPage;
+            var viewModel = new SearchViewModel();
+            viewModel.Build(CurrentPage);
+            
+            return CurrentTemplate(viewModel);
+        }
+        
+        public SearchController(ILogger<RenderController> logger, ICompositeViewEngine compositeViewEngine, IUmbracoContextAccessor umbracoContextAccessor) : base(logger, compositeViewEngine, umbracoContextAccessor)
+        {
+        }
+    }
+}
---
+++ Src/Portfolio.Core/Models/ViewModels/SearchViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Umbraco.Cms.Core.Models.PublishedContent;
+
+namespace Portfolio.Core.Models.ViewModels
+{
+    public class SearchViewModel : MainViewModel
+    {
+        public List<IPublishedContent> Results { get; set; }
+    }
+}
